d89428c313691798b20228cd120f3dd3e1ca0e53
Stephan Goeppentin <stephan.goeppentin@adition.com>
2019-07-30 17:02:08 +0200

    v0.2.0 with flags

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,91 @@
 extern crate all_args_string;
 extern crate string_stupidify;
 
-use all_args_string::*;
 use string_stupidify::*;
+use std::env;
+use string_stupidify::decorators::{Alternate, VaporWave, Shuffle, AlphaSort};
 
 fn main() {
-    let args = all_args_string();
-    let args = args.alternate_case().unwrap();
-    println!("{}", args);
+    let (mut decorators, text) = parse_args();
+    if decorators.is_empty() {
+        decorators = vec![Box::new(Alternate)];
+    }
+    let text = text.decorate(&decorators).unwrap();
+    println!("{}", text);
+}
+
+fn parse_args() -> (Vec<Box<dyn StringDecorator>>, String) {
+    let mut args: Vec<String> = env::args().collect();
+    args.remove(0);
+    let mut in_text = false;
+    let mut text = vec![];
+    let mut decorators = vec![];
+    let mut current_dec: Option<String> = None;
+    let mut current_decorator_args: Vec<String> = vec![];
+    for arg in args {
+        if !in_text {
+            if arg.starts_with("--") {
+                push_optional_decorator(&mut decorators, &current_dec, &current_decorator_args);
+                current_dec = Some(arg.clone());
+            } else if arg.starts_with("-") {
+                current_decorator_args.push(arg.clone());
+            } else {
+                push_optional_decorator(&mut decorators, &current_dec, &current_decorator_args);
+                in_text = true;
+            }
+        }
+        if in_text {
+            text.push(arg.clone());
+        }
+    }
+    (decorators, text.join(" "))
+}
+
+fn push_optional_decorator(decorators: &mut Vec<Box<StringDecorator>>, name: &Option<String>, args: &Vec<String>) {
+    match name {
+        Some(dec) => {
+            match decorator_from_args(&dec, &args) {
+                Some(dec) => decorators.push(dec),
+                None => {},
+            }
+        },
+        None => {},
+    }
+}
+
+fn decorator_from_args(name: &str, _args: &Vec<String>) -> Option<Box<dyn StringDecorator>> {
+    match name {
+        "--bob" => Some(Box::new(Alternate)),
+        "--rev" => Some(Box::new(Reverse)),
+        "--vap" => Some(Box::new(VaporWave)),
+        "--ran" => Some(Box::new(Shuffle)),
+        "--abc" => Some(Box::new(AlphaSort)),
+        "--low" => Some(Box::new(LowerCase)),
+        "--big" => Some(Box::new(UpperCase)),
+        _ => None,
+    }
+}
+
+struct Reverse;
+
+impl StringDecorator for Reverse {
+    fn decorate(&self, text: &String) -> Result<String, DecorationError> {
+        Ok(text.chars().rev().collect())
+    }
+}
+
+struct LowerCase;
+
+impl StringDecorator for LowerCase {
+    fn decorate(&self, text: &String) -> Result<String, DecorationError> {
+        Ok(text.to_lowercase())
+    }
+}
+
+struct UpperCase;
+
+impl StringDecorator for UpperCase {
+    fn decorate(&self, text: &String) -> Result<String, DecorationError> {
+        Ok(text.to_uppercase())
+    }
 }
